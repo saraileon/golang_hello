8e2e2615fbabefc2654ae4ef39b3a8ca1aff5aac
Sarai Leon <sleon90@gmail.com>
2017-05-15 10:34:38 -0500

    testing Gogland + git integration 2

--- main.go
+++ main.go
@@ -3,13 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello, world")
-	fmt.Println(42)
-	// binary %b
-	fmt.Printf("%d  - %b \n", 42, 42)
-	// hexadecimal %x
-	fmt.Printf("%d  - %x \n", 42, 42)
-	fmt.Printf("%d  - %#X \n", 42, 42)
+  fmt.Println("hello, world")
+  fmt.Println(42)
   // binary %b
-	fmt.Printf("%d - %b \n", 394, 394)
+  fmt.Printf("%d  - %b \n", 42, 42)
+  // hexadecimal %x
+  fmt.Printf("%d  - %x \n", 42, 42)
+  fmt.Printf("%d  - %#X \n", 42, 42)
+  // binary %b
+  fmt.Printf("%d - %b \n", 394, 394)
+
+  // for loop decimal, binary, hexadecimal
+  for i := 0; i < 200; i ++ {
+    fmt.Printf("%d \t %b \t %x \n", i, i, i)
+  }
 }
